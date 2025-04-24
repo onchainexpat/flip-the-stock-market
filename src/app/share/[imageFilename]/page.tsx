6b3ccc08967f90b2eb482788185e3a7c7f460208
onchainexpat <onchainexpat@pm.me>
2025-04-24 17:11:29 +0100

    Try again

--- src/app/share/[imageFilename]/page.tsx
+++ src/app/share/[imageFilename]/page.tsx
@@ -20,7 +20,7 @@ export async function generateMetadata({ params }: { params: { imageFilename: st
      };
   }
 
-  const title = "";
+  const title = "💹🧲";
   const description = "Check out this SPX6900 vs S&P 500 price comparison from FlipTheStockMarket.com";
 
   return {
