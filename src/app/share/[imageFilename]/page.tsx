d50bc24a7d2f5afbc9854451363b89398592ab82
onchainexpat <onchainexpat@pm.me>
2025-04-24 17:06:38 +0100

    Remove twitter x post title

--- src/app/share/[imageFilename]/page.tsx
+++ src/app/share/[imageFilename]/page.tsx
@@ -20,7 +20,7 @@ export async function generateMetadata({ params }: { params: { imageFilename: st
      };
   }
 
-  const title = "SPX Price Comparison";
+  const title = "";
   const description = "Check out this SPX6900 vs S&P 500 price comparison from FlipTheStockMarket.com";
 
   return {
